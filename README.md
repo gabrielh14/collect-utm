f109f0434db7b0b9bdb7a48fe865e584d6e94df7
Gabriel Oliveira <gabrielh.net@gmail.com>
2021-06-10 17:10:24 -0300

    :white_check_mark:  add files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # collect-utm
 
-UTM cookies by Gabriel Oliveira
-Script stores the user's UTM information in cookies and uses the information to populate the utm fields in the form
+UTM cookies by Gabriel Oliveira<br />
+Script stores the user's UTM information in cookies and uses the information to populate the utm fields in the form<br />
 Using JavaScript with jQuery
---
+++ collect-utm.js
@@ -0,0 +1,60 @@
+// UTM cookies by Gabriel Oliveira
+// Script stores the user's UTM information in cookies and uses the information to populate the utm fields in the form
+// Using JavaScript with jQuery
+
+jQuery( document ).ready(function() {
+    // check the url for UTM
+    if(window.location.href.indexOf("utm") > -1){
+        // Collect URL Parameters
+        function getParameterByName(name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+    
+        var source = getParameterByName('utm_source');
+        var medium = getParameterByName('utm_medium');
+        var campaign = getParameterByName('utm_campaign');
+        var content = getParameterByName('utm_content');
+        var term = getParameterByName('utm_term');
+    
+        // Store Cookie
+    
+        document.cookie = "utm_source=" + source;
+        document.cookie = "utm_medium=" + medium;
+        document.cookie = "utm_campaign=" + campaign;
+        document.cookie = "utm_content=" + content;
+        document.cookie = "utm_term=" + term;
+    }
+    
+    // Cookie request function
+    function valor_cookie(nome_cookie) {
+        var cname = ' ' + nome_cookie + '=';
+        
+        var cookies = document.cookie;
+        
+        if (cookies.indexOf(cname) == -1) {
+            return false;
+        }
+        
+        cookies = cookies.substr(cookies.indexOf(cname), cookies.length);
+    
+        if (cookies.indexOf(';') != -1) {
+            cookies = cookies.substr(0, cookies.indexOf(';'));
+        }
+        
+        cookies = cookies.split('=')[1];
+        
+        return decodeURI(cookies);
+    }
+    
+    // Collect and store UTM via cookies
+
+    jQuery("#utm_source").val(valor_cookie('utm_source'));
+    jQuery("#utm_medium").val(valor_cookie('utm_medium'));
+    jQuery("#utm_campaign").val(valor_cookie('utm_campaign'));
+    jQuery("#utm_term").val(valor_cookie('utm_term'));
+    jQuery("#utm_content").val(valor_cookie('utm_content'));
+    
+});
